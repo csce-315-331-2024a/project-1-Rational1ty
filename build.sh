da92ebd966fabd11a1a40c767d479c08393bd41d
Matthew Davidson <mdavidson390@outlook.com>
2026-07-09 15:44:44 -0500

    Add helper scripts

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p build
+
+cp -uv *.html build
+cp -uvr styles build
+cp -uvr scripts build
+cp -uvr assets build
---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if ! [[ -d '/u' ]]; then
+	echo "fatal: Can't deploy if '/u' drive is not mounted" >&2
+	exit 1
+fi
+
+echo "Deploying build/"
+
+echo -e "\nThese files will be made public at https://people.tamu.edu/~mdavidson:"
+# list all the files in the build/ folder, sort them by nesting level, and prepend "+"
+find build -type f | awk -F/ '{print NF-1,$0}' | sort -n | cut -d' ' -f2- | sed 's|^|  + |'
+
+echo -e "\nThese files will no longer be public and will be moved to /u/backup":
+find /u/public_html -type f | awk -F/ '{print NF-1,$0}' | sort -n | cut -d' ' -f2- | sed 's|^|  - |'
+
+echo
+read -p "Are you sure you want to continue (y/n)? " -n 1
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Aborting (no files modified)"
+	exit 0
+fi
+
+cp -vr /u/public_html/* /u/backup && cp -vr build/* /u/public_html
+
+if [[ $? -ne 0 ]]
+then
+	echo "Failed to deploy"
+	exit 1
+fi
+
+echo -e "\nWebsite published; previous version saved in /u/backup"
